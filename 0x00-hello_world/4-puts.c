ff7e7e28d16afce578b0cbde3fbd07133ed2dc2d
Abrsh7 <abrshn7@gmail.com>
2022-12-08 13:15:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main- serves as the entry point to the program
  * Return: 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle")		return (0);
 }
