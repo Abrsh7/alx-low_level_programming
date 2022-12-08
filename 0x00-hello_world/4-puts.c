60154727d518010f6509e8218cd269152069bd8d
Abrsh7 <abrshn7@gmail.com>
2022-12-08 12:07:50 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - entry
+ * main -entry
  * return -always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
