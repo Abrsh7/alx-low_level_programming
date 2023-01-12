9678a9d1b968ac213f412256c74e6c6b34840a9c
Abrsh7 <abrshn7@gmail.com>
2023-01-11 16:47:28 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * _strdup - a function to copy a string on a new allocated memory
+ * area .
+ * @str: string to copy
+ *
+ * Return: a pointer to the copied string .
  */
+
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+char *pointeur;
+int i;
+int count = 0;
 
-	if (str == NULL)
-		return (NULL);
+/* in case if the given string is NULL*/
+if (str == NULL)
+	return (NULL);
 
-	for (end = 0; end <= *str; end++)
-	{
-	}
+/* finding the length of the given string*/
+while (*(str + count))
 
-	end += 1;
-	array = malloc(sizeof(char) * end);
+	count++;
+/*allocation the needed dynamic memory area */
+pointeur = malloc(sizeof(char) * (count + 1));
 
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
 
-	if (array == NULL)
-		return (NULL);
-	return (array);
+/* to verify if the pointer , pointed on the allocated memory*/
+/* area is not NULL */
+if (pointeur == NULL)
+	return (NULL);
+
+/* initialization of i to avoid segment fault error*/
+i = 0;
+
+/* filling the allocated area with the bytes from the given string */
+while (i <= count)
+{
+	*(pointeur + i) = *(str + i);
+		i++;
+}
+
+/* return a pointer to the string */
+return (pointeur);
 }
